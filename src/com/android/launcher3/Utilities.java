7b8104819b0f0dc8033303fc1f3975d92b0a4c1c
magicxavi <rolfpeterkesche@gmail.com>
2025-03-05 11:41:53 +0200

    def opacity 0

--- src/com/android/launcher3/Utilities.java
+++ src/com/android/launcher3/Utilities.java
@@ -1133,7 +1133,7 @@ public final class Utilities {
 
     public static int getRecentsOpacity(Context context) {
         SharedPreferences prefs = LauncherPrefs.getPrefs(context.getApplicationContext());
-        return prefs.getInt(KEY_RECENTS_OPACITY, 40);
+        return prefs.getInt(KEY_RECENTS_OPACITY, 0);
     }
 
     public static int getAllAppsOpacity(Context context) {
